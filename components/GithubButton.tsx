e875002ad66dc7f7b5e3087fc90a02a417214774
shadowoff09 <diogogaspar931@gmail.com>
2025-02-23 05:27:26 +0000

    Enhance GitHub button styling with improved hover and dark mode effects

--- components/GithubButton.tsx
+++ components/GithubButton.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 export default function GithubButton() {
   return (
     <div>
-        <Button variant="ghost" size="icon" asChild>
+        <Button variant="ghost" size="icon" asChild className="relative overflow-hidden transition-colors duration-300 ease-in-out hover:text-accent-foreground cursor-pointer hover:bg-background/20 hover:dark:bg-background/60 backdrop-blur-sm">
           <Link href="https://github.com/shadowoff09">
-            <FaGithub />
+            <FaGithub className="dark:text-white" />
           </Link>
         </Button>
     </div>
