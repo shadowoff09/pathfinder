ce5c921994ceab96c8262994c6ac0abf9cea9d00
Diogo Gaspar <diogogaspar931@gmail.com>
2025-02-23 15:52:28 +0000

    Update GitHub button with external link and text color adjustment

--- components/GithubButton.tsx
+++ components/GithubButton.tsx
@@ -6,8 +6,8 @@ export default function GithubButton() {
   return (
     <div>
         <Button variant="ghost" size="icon" asChild className="relative overflow-hidden transition-colors duration-300 ease-in-out hover:text-accent-foreground cursor-pointer hover:bg-background/20 hover:dark:bg-background/60 backdrop-blur-sm">
-          <Link href="https://github.com/shadowoff09">
-            <FaGithub className="dark:text-white" />
+          <Link href="https://github.com/shadowoff09" target="_blank">
+            <FaGithub className="text-white" />
           </Link>
         </Button>
     </div>
