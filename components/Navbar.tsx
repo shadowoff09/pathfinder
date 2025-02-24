ca2850d2b92cc32477c0d4bbbebd831b1caa34b9
Diogo Gaspar <diogogaspar931@gmail.com>
2025-02-24 18:24:03 +0000

    Improve RouteIcon hover state in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
   return (
     <nav className="bg-black/10 backdrop-blur-sm shadow-md px-4 py-3 absolute w-full top-0 z-50">
       <div className="max-w-7xl mx-auto flex flex-wrap items-center justify-between">
-        <div className="flex items-center text-white">
-          <RouteIcon className="h-9 w-9 mr-2" />
+        <div className="flex items-center text-white ">
+          <RouteIcon className="h-9 w-9 mr-2 hover:bg-transparent" />
           <span className="text-xl font-semibold font-onest">Pathfinder</span>
         </div>
         <div className="flex items-center space-x-2">
