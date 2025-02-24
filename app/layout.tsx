c0df584e7134183d83ae610790504d0dcfdacc2a
Diogo Gaspar <diogogaspar931@gmail.com>
2025-02-24 18:17:40 +0000

    Add base URL and improve metadata descriptions for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,20 +21,23 @@ const onest = Onest({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://pathfinder.shadowdev.xyz"),
   title: "Pathfinder",
-  description: "Pathfinder",
+  description: "Pathfinder is a tool that helps you find the best path for your journey.",
   icons: [
     { rel: "icon", url: "/images/favicon.ico" },
   ],
   openGraph: {
+    url: "https://pathfinder.shadowdev.xyz",
+    siteName: "Pathfinder",
     title: "Pathfinder",
-    description: "Pathfinder",
+    description: "Pathfinder is a tool that helps you find the best path for your journey.",
     images: "/banner.png",
   },
   twitter: {
     card: "summary_large_image",
     title: "Pathfinder",
-    description: "Pathfinder",
+    description: "Pathfinder is a tool that helps you find the best path for your journey.",
     images: "/banner.png",
   },
 };
